033731351916d39014681852cc895f2b7ee35098
guooujie <guooujie@163.com>
2022-09-02 22:15:55 +0800

    完成proto版本的运行

---
+++ modules/calc/calc_cyber.cpp
@@ -0,0 +1,42 @@
+
+#include "modules/calc/include/vnet.h"
+#include "modules/calc/include/vtcp.h"
+#include "modules/calc/include/vudp.h"
+#include "modules/calc/include/cfgbus.h"
+#include "modules/calc/include/k_project.h"
+#include "modules/calc/include/k_command.h"
+#include "modules/calc/include/k_process.h"
+#include "modules/calc/include/k_lib.h"
+
+#include "cyber/cyber.h"
+#include "cyber/timer/timer.h"
+
+#include <iostream>
+#include <memory>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  apollo::cyber::Init(argv[0]);
+
+  vnet_init();
+  vtcp_init();
+  cfgbus_init(1);
+  vudp_init();
+  // io_init();
+  lib_init();
+  prj_init(1);
+
+  uint64_t interval_ = 1000;
+  std::unique_ptr<apollo::cyber::Timer> timer_;
+  auto func = []() {
+    prj_exec();
+    ev_dump();
+  };
+  timer_.reset(new apollo::cyber::Timer(interval_, func, false));
+  timer_->Start();
+
+  apollo::cyber::WaitForShutdown();
+
+  return 0;
+}
