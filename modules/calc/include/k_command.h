a28e719e262e7f8d1fab355f0f50123c06c6270c
guooujie@163.com <guooujie@163.com>
2022-09-20 15:14:30 +0800

    modify

--- modules/calc/include/k_command.h
+++ modules/calc/include/k_command.h
@@ -1,19 +1,16 @@
 #ifndef k_command_h
 #define k_command_h
-// #ifdef __cplusplus
-// extern "C" {
-// #endif
 
-int cmd_dispatch(char *line);
-int cmds_dispatch(char *buf);
+#include "modules/calc/proto/cmd.pb.h"
+
+int cmd_dispatch(Cmd::EditInfo &edit_info);
+int cmds_dispatch(Cmd::EditInfos &edit_infos);
 
 int cmds_reset();
 int cmds_load(char *buf, int len);
 int cmds_append(char *buf, int len);
 
-// #ifdef __cplusplus
-// }
-// #endif
+
 #endif
 
 
