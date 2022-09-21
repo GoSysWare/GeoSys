f50d976b6717bf3f2023e942a7c36fb587cab0e7
guooujie@163.com <guooujie@163.com>
2022-09-21 10:55:53 +0800

    modify

---
+++ modules/calc/common/calc_gflags.cpp
@@ -0,0 +1,5 @@
+#include "calc_gflags.h"
+
+
+// DEFINE_bool(big_menu, true, "Include 'advanced' options in the menu listing");
+DEFINE_string(calc_edit_cmd_name, "/calc/kernel/cmd/edit","balabala");
---
+++ modules/calc/common/calc_gflags.h
@@ -0,0 +1,7 @@
+#ifndef k_gflags_h
+#define k_gflags_h
+
+#include <gflags/gflags.h>
+
+DECLARE_String(calc_edit_cmd_name);
+#endif
